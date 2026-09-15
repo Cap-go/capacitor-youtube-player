51a3c252011e5adcde9eff9b6d41643f2ff9aa82
Martin DONADIEU <martindonadieu@gmail.com>
2026-09-15 08:21:34 +0200

    ci: add Cap 9 deprecated-API guard (#65)

--- android/src/test/java/com/capgo/youtubeplayer/YoutubePlayerJsResultParserTest.java
+++ android/src/test/java/com/capgo/youtubeplayer/YoutubePlayerJsResultParserTest.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 import org.json.JSONObject;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
+@RunWith(RobolectricTestRunner.class)
 public class YoutubePlayerJsResultParserTest {
 
     @Test
